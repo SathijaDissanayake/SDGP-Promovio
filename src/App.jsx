6787f56a9263a718cbb22261abe82efd0580ab56
Apoorwa2004 <methmi.20231004@iit.ac.lk>
2025-02-25 13:37:23 +0530

    PricingPage Implementation

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/LandingPage/Header";
 import LandingPage from "./components/LandingPage/LandingPage";
-
+import PricingPage from "./components/PricingPage/PricingPage";
+import CheckoutPage from "./components/PricingPage/CheckoutPage";
 
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
       <Header/>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        
+        <Route path="/Pricing" element={<PricingPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
       </Routes>
     </Router>
   );
---
+++ src/components/PricingPage/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <div>
+      <footer className="footer">
+              <div className="container">
+                  <p>&copy; 2025 Promovio. All rights reserved.</p>
+                  <p>Trusted by the all innovative teams</p>
+              </div>
+            </footer>
+    </div>
+  )
+}
